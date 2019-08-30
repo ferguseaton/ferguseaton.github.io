0fc659b12c25c85f90ad7e060b0c363ba8f5b83e
ferguseaton <48812615+ferguseaton@users.noreply.github.com>
2019-08-30 10:17:39 +1000

    add future signals

---
+++ future-signals/assets/js/main.js
@@ -0,0 +1,47 @@
+$(document).ready(function(){
+	console.log("The page is ready!");
+
+	$('.research-item').click(function(){
+		$(this).toggleClass('large');
+	});
+
+	$('.nav-item').click(function(){
+		$('.nav-item').removeClass('active')
+		$(this).addClass('active')
+	});
+
+	var hideAll = function(){
+		$('.research-item').addClass('hide')
+	}
+
+	$('.btn-playstation').click(function(){
+		hideAll();
+		$('.playstation').removeClass('hide')
+	});
+
+	$('.btn-pc').click(function(){
+		hideAll();
+		$('.pc').removeClass('hide')
+	});
+
+	$('.btn-xbox').click(function(){
+		hideAll();
+		$('.xbox').removeClass('hide')
+	})
+
+	$('.btn-nintendo').click(function(){
+		hideAll();
+		$('.nintendo').removeClass('hide')
+	})
+
+	$('.btn-mobile').click(function(){
+		hideAll();
+		$('.mobile').removeClass('hide')
+	})
+
+	$('.btn-nofilter').click(function(){
+		hideAll();
+		$('.research-item').removeClass('hide')
+	})
+
+});
